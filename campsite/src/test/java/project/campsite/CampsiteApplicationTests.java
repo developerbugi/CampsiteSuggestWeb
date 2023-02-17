7cf4f6710a817333a5070aa4ec0dc39694bd9e8f
거부길 <sopens@naver.com>
2023-02-18 02:33:26 +0900

    개발 초기 설정

---
+++ campsite/src/test/java/project/campsite/CampsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package project.campsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
