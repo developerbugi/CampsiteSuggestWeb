e1fc43c5f77cfbcb54bd4a4764db9195dacdd0b8
거부길 <sopens@naver.com>
2023-02-19 03:25:51 +0900

    h2 DB 연결 테스트

---
+++ campsite/src/test/java/project/campsite/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package project.campsite.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import project.campsite.domain.Member;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testDB() {
+        Member member = new Member();
+        member.setUsername("부길");
+
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        Assertions.assertThat(member.getId()).isEqualTo(findMember.getId());
+        Assertions.assertThat(member.getUsername()).isEqualTo(findMember.getUsername());
+    }
+}
