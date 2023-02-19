eff4a474e476ead398de201f9f8d3cdc6d6ef460
거부길 <sopens@naver.com>
2023-02-20 04:03:07 +0900

    🙋🏻‍♂️ MemberController
    미완성

---
+++ campsite/src/main/java/project/campsite/controller/MemberForm.java
@@ -0,0 +1,32 @@
+package project.campsite.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import project.campsite.domain.member.Address;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter @Setter
+public class MemberForm {
+
+    @NotBlank(message = "회원 이름을 입력해주세요")
+    @Size(min = 3, max = 10, message = "이름은 3 ~ 10자 이여야 합니다!")
+    private String username;
+    @NotBlank(message = "전화번호를 입력해주세요")
+    private String number;
+
+
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    private String password;
+
+    private String birth;
+    
+    private String city;
+    private String street;
+    private String Zipcode;
+}
