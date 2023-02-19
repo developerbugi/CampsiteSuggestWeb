3e4e4336dc6938ed7c8115b8525914437cdd121d
거부길 <sopens@naver.com>
2023-02-20 04:03:38 +0900

    🙋🏻‍♂️ MemberRepository

--- campsite/src/main/java/project/campsite/repository/MemberRepository.java
+++ campsite/src/main/java/project/campsite/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package project.campsite.repository;
 
 import org.springframework.stereotype.Repository;
-import project.campsite.domain.Member;
+import project.campsite.domain.member.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -18,5 +19,27 @@ public class MemberRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    /**
+     * 전체 멤버 조회
+     * @return
+     */
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    /**
+     * 단일 멤버 조회
+     * @param id
+     * @param name
+     * @return
+     */
+    public List<Member> findByName(Long id, String name){
+        return em.createQuery("select m from Member m where m.id=?1 and m.name =?2", Member.class)
+                .setParameter(1, id)
+                .setParameter(2, name)
+                .getResultList();
     }
 }
